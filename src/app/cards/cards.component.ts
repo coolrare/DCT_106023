86d0ed7ba176703d7a3fe22444bb035ac65af2ca
Will保哥 <doggy.huang@gmail.com>
2017-06-11 14:20:18 +0800

    09 練習在同一個路由之間做切換，只有參數改變、路由不變

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -14,12 +14,15 @@ export class CardsComponent implements OnInit {
 
   ngOnInit() {
 
-    this.type = this.route.snapshot.params['type'];
+    // this.type = this.route.snapshot.params['type'];
 
-    // this.route.params.subscribe(params => {
-    //   this.type = params['type'];
-    // });
+    this.route.params.subscribe(params => {
+      this.type = params['type'];
+    });
 
   }
 
+  goCards(num) {
+    this.router.navigate(['/', 'cards', parseInt(this.type) + num]);
+  }
 }
