feceb80da796b28cc20fe563a3e08a1f9b5a3dad
Will保哥 <doggy.huang@gmail.com>
2017-06-11 13:17:46 +0800

    建立 charts/flot 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CardsComponent
+    CardsComponent,
+    FlotComponent
   ],
   imports: [
     BrowserModule,
