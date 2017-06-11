1c8520deece05bc14950531c7807b248352b53ae
Will保哥 <doggy.huang@gmail.com>
2017-06-11 10:52:14 +0800

    05 練習預設路由、轉向路由、萬用路由與將特定路由獨立成常數或 TS 模組

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Route, Routes,  RouterModule } from '@angular/router';
+import { Routes,  RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
-
-const fallbackRoute: Route = {
-  path: '**',
-  redirectTo: '/page1',
-  pathMatch: 'full'
-};
+import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: '', redirectTo: 'page1', pathMatch: 'full' },
---
+++ src/app/shared/fallback-route.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+
+export const fallbackRoute: Route = {
+  path: '**',
+  redirectTo: '/page1',
+  pathMatch: 'full'
+};
