51c172c4d620e47af3c662eb1f8bf96810483e2b
Will保哥 <doggy.huang@gmail.com>
2017-06-18 10:31:20 +0800

    16 練習 Angular 表單的 NgForm 用法

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-classic',
@@ -12,6 +13,18 @@ export class ClassicComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.data = {
+      'title': 'This is title',
+      'subtitle': 'Hello World'
+    };
+  }
+
+  doSubmit(form: NgForm, $event) {
+    console.log(form);
+    console.log($event);
+    if (form.invalid) {
+      alert('表單無效，請檢查欄位!');
+    }
   }
 
 }
