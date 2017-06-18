0e1b089bec702081a50f3fcdd03606e931ac4fc7
Will保哥 <doggy.huang@gmail.com>
2017-06-18 13:17:57 +0800

    19 練習套用 Reactive Forms 表單模型

--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  data: any = {};
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      'title': 'This is title',
+      'subtitle': 'Hello'
+    });
   }
 
 }
